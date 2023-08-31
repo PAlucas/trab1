bf81d7096dc65976431b5776014c96d5c71e0a26
Lucas Perlatto Lotti Garcia <44738000+PAlucas@users.noreply.github.com>
2023-08-31 12:04:02 -0300

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -14,7 +14,7 @@ def repoUrl = 'https://github.com/PAlucas/trab1.git'
 def gettags = ("git ls-remote --tags ${repoUrl}").execute()
 
 List list = gettags.text.readLines().collect { 
-    it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '').replaceAll("\^\{\}", '')
+    it.split()[1].replaceAll('refs/heads/', '').replaceAll('refs/tags/', '')
 }
 
 list.sort { x, y -> 
